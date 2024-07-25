45e9c14cd20ed884593c9dc71d9151fad3ee44a7
hamzamalik22 <hamzamalikaj22@gmail.com>
2024-07-25 16:44:42 +0500

    Image hosting v4.1

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -24,9 +24,25 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Path to your Firebase Admin SDK JSON file
-firebase_creds = os.path.join(
-    BASE_DIR, "config", "umer-portfolio-23756-firebase-adminsdk-4owq0-687f5bf218.json"
-)
+# firebase_creds = os.path.join(
+#     BASE_DIR, "config", "umer-portfolio-23756-firebase-adminsdk-4owq0-687f5bf218.json"
+# )
+# cred = credentials.Certificate(firebase_creds)
+
+firebase_creds = {
+    "type": os.getenv("FIREBASE_TYPE"),
+    "project_id": os.getenv("FIREBASE_PROJECT_ID"),
+    "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
+    "private_key": os.getenv("FIREBASE_PRIVATE_KEY").replace("\\n", "\n"),
+    "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
+    "client_id": os.getenv("FIREBASE_CLIENT_ID"),
+    "auth_uri": os.getenv("FIREBASE_AUTH_URI"),
+    "token_uri": os.getenv("FIREBASE_TOKEN_URI"),
+    "auth_provider_x509_cert_url": os.getenv("FIREBASE_AUTH_PROVIDER_X509_CERT_URL"),
+    "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_X509_CERT_URL"),
+    "universe_domain": os.getenv("FIREBASE_UNIVERSE_DOMAIN"),
+}
+
 cred = credentials.Certificate(firebase_creds)
 firebase_admin.initialize_app(
     cred, {"storageBucket": "umer-portfolio-23756.appspot.com"}
